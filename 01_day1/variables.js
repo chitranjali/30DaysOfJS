00cc2fb8f9daadede20ad4b63afbb2e96578036e
chitranjali <chitranjali189@gmail.com>
2020-04-02 15:25:28 +0530

    day1

---
+++ 01_day1/variables.js
@@ -0,0 +1,20 @@
+//comments can make code readable
+//welcome to 30DaysOfJavaScript
+
+/*comments can make code readable,
+ easy to use and informative
+ */
+
+let name = 'chitra'
+let lop = undefined;
+let notKnown = null;
+
+let firstName = 'chitranjali',
+lastName = 'edpuganti',
+status = 'single',
+country = 'India'
+
+console.log(typeof name);
+console.log(typeof status);
+console.log(typeof lop);
+console.log(typeof notKnown);
