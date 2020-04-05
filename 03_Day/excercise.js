b7237f8012c967fb91bc200bae6255fb7ba30c17
chitranjali <chitranjali189@gmail.com>
2020-04-05 15:16:48 +0530

    day3

---
+++ 03_Day/excercise.js
@@ -0,0 +1,42 @@
+if (typeof '10' === 10){
+    console.log('rue')
+}else{
+    console.log('False')
+}
+
+if ('10'){
+    console.log('True')
+}
+
+// datetoday = new Date();
+// console.log(datetoday.getMonth());
+// console.log(datetoday.getDay());
+
+const now = new Date()
+const year = now.getFullYear() // return year
+const month = now.getMonth() + 1 // return month(0 - 11)
+const date = now.getDate() // return date (1 - 31)
+const hours = now.getHours() // return number (0 - 23)
+const minutes = now.getMinutes() // return number (0 -59)
+
+console.log(`${date}`)
+console.log(`${date}/${month}/${year} ${hours}:${minutes}`);
+
+input = prompt("enter base and height");
+val = input.split(',');
+area = 0.5 * val[0] * val[1];
+console.log(area);
+
+//Slope is (m = y2-y1/x2-x1). Find the slope between point (2, 2) and point(6,10)
+var coordinates = new Array();
+coordinates.push(new coordinate(2, 2));
+coordinates.push(new coordinate(6, 10));
+
+function coordinate(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+m = (coordinates[1].y - coordinates[0].y) / (coordinates[1].x - coordinates[0].x)
+console.log(m);
+
---
+++ 03_Day/readme.md
@@ -0,0 +1,2 @@
+As rule of thumb, if a value is not true with == it will not be equal with ===. Using === is safer than using ==. 
+The following link has an exhaustive list of comparison of data types
